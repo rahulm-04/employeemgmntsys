75c50ea6c3cfa7dc59f290a10c12087a7dba2a2b
rahulm-04 <raahulix@gmail.com>
2025-04-16 15:27:29 +0530

    changes

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -15,8 +15,9 @@ const Login = ({handleLogin}) => {
 
 
   return (
-    <div className='flex h-screen w-screen items-center justify-center'>
+    <div className='flex h-screen w-screen items-center justify-center' >
         <div className='border-2 rounded-xl border-emerald-600 p-20'>
+        <h1 className="text-2xl font-bold text-center text-emerald-600 mb-6">Login To Your Account </h1>
             <form onSubmit={(e)=>{submitHandler(e)}} className='flex flex-col items-center justify-center'>
                 <input value={email} onChange={(e)=>{setEmail(e.target.value)}} required className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email' />
                 <input value={password} onChange={(e)=>{setPassword(e.target.value)}} required className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400' type="password" placeholder='Enter password' />
